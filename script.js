3263f86f2f201720688418872b4cf58c1ef39482
alexsylvian <lazeratus1@gmail.com>
2025-06-08 19:34:49 -0400

    random ai complete

--- script.js
+++ script.js
@@ -166,40 +166,142 @@ function createChessBoard() {
     setTimeout(aiMove, 500);
   }
 
-  function aiMove() {
-    const possibleMoves = [];
-    for (let row = 0; row < 8; row++) {
-      for (let col = 0; col < 8; col++) {
-        if (boardState[row][col] === "♟") {
-          const forward = row + 1;
+function aiMove() {
+  const possibleMoves = [];
 
-          if (forward < 8 && !boardState[forward][col]) {
-            possibleMoves.push({ from: [row, col], to: [forward, col] });
+  const isOpponent = (piece) => piece && !["♟", "♜", "♞", "♝", "♛", "♚"].includes(piece);
+
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      const piece = boardState[row][col];
+      if (!piece) continue;
+
+      const forward = row + 1;
+
+      if (piece === "♟") {
+        if (forward < 8 && !boardState[forward][col]) {
+          possibleMoves.push({ from: [row, col], to: [forward, col] });
+        }
+
+        if (forward < 8 && col > 0 && isOpponent(boardState[forward][col - 1])) {
+          possibleMoves.push({ from: [row, col], to: [forward, col - 1] });
+        }
+
+        if (forward < 8 && col < 7 && isOpponent(boardState[forward][col + 1])) {
+          possibleMoves.push({ from: [row, col], to: [forward, col + 1] });
+        }
+      }
+
+      if (piece === "♜") {
+        const directions = [[1,0], [-1,0], [0,1], [0,-1]];
+        for (let [dr, dc] of directions) {
+          for (let i = 1; i < 8; i++) {
+            const r = row + dr * i;
+            const c = col + dc * i;
+            if (r < 0 || r > 7 || c < 0 || c > 7) break;
+            const target = boardState[r][c];
+            if (!target) {
+              possibleMoves.push({ from: [row, col], to: [r, c] });
+            } else if (isOpponent(target)) {
+              possibleMoves.push({ from: [row, col], to: [r, c] });
+              break;
+            } else {
+              break;
+            }
           }
+        }
+      }
 
-          if (forward < 8 && col > 0 && boardState[forward][col - 1] && boardState[forward][col - 1] !== "♟") {
-            possibleMoves.push({ from: [row, col], to: [forward, col - 1] });
+      if (piece === "♞") {
+        const moves = [
+          [2, 1], [2, -1], [-2, 1], [-2, -1],
+          [1, 2], [1, -2], [-1, 2], [-1, -2]
+        ];
+        for (let [dr, dc] of moves) {
+          const r = row + dr;
+          const c = col + dc;
+          if (r >= 0 && r < 8 && c >= 0 && c < 8) {
+            const target = boardState[r][c];
+            if (!target || isOpponent(target)) {
+              possibleMoves.push({ from: [row, col], to: [r, c] });
+            }
           }
+        }
+      }
 
-          if (forward < 8 && col < 7 && boardState[forward][col + 1] && boardState[forward][col + 1] !== "♟") {
-            possibleMoves.push({ from: [row, col], to: [forward, col + 1] });
+      if (piece === "♝") {
+        const directions = [[1,1], [1,-1], [-1,1], [-1,-1]];
+        for (let [dr, dc] of directions) {
+          for (let i = 1; i < 8; i++) {
+            const r = row + dr * i;
+            const c = col + dc * i;
+            if (r < 0 || r > 7 || c < 0 || c > 7) break;
+            const target = boardState[r][c];
+            if (!target) {
+              possibleMoves.push({ from: [row, col], to: [r, c] });
+            } else if (isOpponent(target)) {
+              possibleMoves.push({ from: [row, col], to: [r, c] });
+              break;
+            } else {
+              break;
+            }
+          }
+        }
+      }
+
+      if (piece === "♛") {
+        const directions = [
+          [1,0], [-1,0], [0,1], [0,-1],
+          [1,1], [1,-1], [-1,1], [-1,-1]
+        ];
+        for (let [dr, dc] of directions) {
+          for (let i = 1; i < 8; i++) {
+            const r = row + dr * i;
+            const c = col + dc * i;
+            if (r < 0 || r > 7 || c < 0 || c > 7) break;
+            const target = boardState[r][c];
+            if (!target) {
+              possibleMoves.push({ from: [row, col], to: [r, c] });
+            } else if (isOpponent(target)) {
+              possibleMoves.push({ from: [row, col], to: [r, c] });
+              break;
+            } else {
+              break;
+            }
+          }
+        }
+      }
+
+      if (piece === "♚") {
+        for (let dr = -1; dr <= 1; dr++) {
+          for (let dc = -1; dc <= 1; dc++) {
+            if (dr === 0 && dc === 0) continue;
+            const r = row + dr;
+            const c = col + dc;
+            if (r >= 0 && r < 8 && c >= 0 && c < 8) {
+              const target = boardState[r][c];
+              if (!target || isOpponent(target)) {
+                possibleMoves.push({ from: [row, col], to: [r, c] });
+              }
+            }
           }
         }
       }
     }
-  
-    if (possibleMoves.length > 0) {
-      const move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
-      const [fromRow, fromCol] = move.from;
-      const [toRow, toCol] = move.to;
-      boardState[toRow][toCol] = boardState[fromRow][fromCol];
-      boardState[fromRow][fromCol] = "";
-      moveSound.play()
-    }
-  
-    currentPlayer = "white";
-    renderChessBoard();
-  }  
+  }
+
+  if (possibleMoves.length > 0) {
+    const move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)];
+    const [fromRow, fromCol] = move.from;
+    const [toRow, toCol] = move.to;
+    boardState[toRow][toCol] = boardState[fromRow][fromCol];
+    boardState[fromRow][fromCol] = "";
+    moveSound.play();
+  }
+
+  currentPlayer = "white";
+  renderChessBoard();
+}
 
   function renderChessBoard() {
     chessBoard.innerHTML = "";
